adcc02ee05322d516618cb12cbf5b0559700d3e8
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-03-24 15:55:34 -0300

    Back end - Aula 16.Exceção para CPF já cadastrado

--- helpdesk/src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ helpdesk/src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.matheus.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -23,6 +24,18 @@ public class ResourceExceptionHandler {
 				ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandarError> dataIntegrityViolationException(DataIntegrityViolationException ex, 
+			HttpServletRequest request){
+		
+		StandarError error = new StandarError(System.currentTimeMillis(), 
+				HttpStatus.BAD_REQUEST.value(), "Violação de dados",
+				ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 		
 	}
 	
