3b9c787100ec955e80cf3aa2d3440dcd1265dd1a
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-03-24 16:42:43 -0300

    Back end - Aula 17.Validations

--- helpdesk/src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ helpdesk/src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.matheus.helpdesk.resources.exceptions;
 
-import java.time.LocalDateTime;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -36,6 +36,25 @@ public class ResourceExceptionHandler {
 				ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		
+	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandarError> validationErrors(MethodArgumentNotValidException ex, 
+			HttpServletRequest request){
+		
+		ValidationError errors = new ValidationError(System.currentTimeMillis(),
+				HttpStatus.BAD_REQUEST.value(), "Validation error",
+				"Erro na validação dos campos", request.getRequestURI());
+		
+		
+		for(FieldError x: ex.getBindingResult().getFieldErrors()) {
+			errors.addErrors(x.getField(), x.getDefaultMessage());
+		}
+		
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 		
 	}
 	
