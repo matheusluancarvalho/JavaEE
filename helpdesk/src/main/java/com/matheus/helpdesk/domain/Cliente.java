333390566a7bfa43a3ac260c1eaff7bf1067e88f
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-03-25 15:05:03 -0300

    Back end - Aula 20.CRUD para Cliente

--- helpdesk/src/main/java/com/matheus/helpdesk/domain/Cliente.java
+++ helpdesk/src/main/java/com/matheus/helpdesk/domain/Cliente.java
@@ -2,17 +2,19 @@ package com.matheus.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.matheus.helpdesk.domain.dtos.ClienteDTO;
 import com.matheus.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
@@ -27,6 +29,18 @@ public class Cliente extends Pessoa {
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Cliente(ClienteDTO obj) {
+		super();
+
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
@@ -34,7 +48,5 @@ public class Cliente extends Pessoa {
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
-	
-	
-	
+
 }
--- helpdesk/src/main/java/com/matheus/helpdesk/services/ClienteService.java
+++ helpdesk/src/main/java/com/matheus/helpdesk/services/ClienteService.java
@@ -9,59 +9,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheus.helpdesk.domain.Pessoa;
-import com.matheus.helpdesk.domain.Tecnico;
-import com.matheus.helpdesk.domain.dtos.TecnicoDTO;
+import com.matheus.helpdesk.domain.Cliente;
+import com.matheus.helpdesk.domain.dtos.ClienteDTO;
 import com.matheus.helpdesk.repositories.PessoaRepository;
-import com.matheus.helpdesk.repositories.TecnicoRepository;
+import com.matheus.helpdesk.repositories.ClienteRepository;
 import com.matheus.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class TecnicoService {
+public class ClienteService {
 
 	@Autowired
-	private TecnicoRepository repository;
+	private ClienteRepository repository;
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
-	public Tecnico findById(Integer id) {
-		Optional<Tecnico> obj = repository.findById(id);
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 
-	public List<Tecnico> findAll() {
+	public List<Cliente> findAll() {
 		return repository.findAll();
 	}
 
-	public Tecnico create(TecnicoDTO objDTO) {
+	public Cliente create(ClienteDTO objDTO) {
 		objDTO.setId(null);
 		validaPorCpfEEmail(objDTO);
-		Tecnico newObj = new Tecnico(objDTO);
+		Cliente newObj = new Cliente(objDTO);
 		return repository.save(newObj);
 	}
 
-	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
 		objDTO.setId(id);
-		Tecnico oldObj = findById(id);
+		Cliente oldObj = findById(id);
 		validaPorCpfEEmail(objDTO);
-		oldObj = new Tecnico(objDTO);
+		oldObj = new Cliente(objDTO);
 
 		return repository.save(oldObj);
 	}
 
 	public void delete(Integer id) {
-		Tecnico obj = findById(id);
+		Cliente obj = findById(id);
 
 		if (obj.getChamados().size() > 0) {
 			throw new DataIntegrityViolationException(
-					"Tecnico possui chamados em aberto" + " e não pode ser deletado no momento!");
+					"Cliente possui chamados em aberto" + " e não pode ser deletado no momento!");
 		}
 		repository.deleteById(id);
 
 	}
 
-	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
